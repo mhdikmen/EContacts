449b802a501b7702f9575a03f526500fe9e84872
Muhammed Hanifi Dikmen <dikmen@live.com>
2025-01-05 07:23:11 +0300

    Report.API conf updated

--- src/Services/Report/Report.API/Services/Contact/ContactService.cs
+++ src/Services/Report/Report.API/Services/Contact/ContactService.cs
@@ -6,9 +6,11 @@ namespace Report.API.Services.Contact
 {
     public class ContactService : IContactService
     {
+        private readonly ILogger<ContactService> _logger;
         private readonly IHttpClientFactory _httpClientFactory;
-        public ContactService(IHttpClientFactory clientFactory)
+        public ContactService(ILogger<ContactService> logger, IHttpClientFactory clientFactory)
         {
+            _logger = logger;
             _httpClientFactory = clientFactory;
         }
         public async Task<IList<ContactDto>> GetReportsAsync()
@@ -19,6 +21,7 @@ namespace Report.API.Services.Contact
             long currentPageIndex = 0;
             do
             {
+                _logger.LogInformation($"Fetching started for contacts from page {currentPageIndex}");
                 HttpClient client = _httpClientFactory.CreateClient("Contact");
                 HttpResponseMessage response = await client.GetAsync($"contacts?pageIndex={currentPageIndex}&pageSize={pageSize}");
                 response.EnsureSuccessStatusCode();
@@ -28,7 +31,7 @@ namespace Report.API.Services.Contact
 
                 if ((resp?.Contacts?.Data?.Count()).GetValueOrDefault() != 0)
                     contactDtos.AddRange(resp!.Contacts.Data);
-
+                _logger.LogInformation($"Fetching completed for contacts from page {currentPageIndex}");
                 currentPageIndex++;
             } while (currentPageIndex < pageCount);
 
